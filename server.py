597caba87ffe8a5f08991dbe6f3c32bb00b1c59c
PhenixMessenger <sodykovtagir@gmail.com>
2026-06-19 04:53:04 +0300

    Update server.py

--- server.py
+++ server.py
@@ -112,4 +112,6 @@ def browse():
             return f"Ошибка шлюза: {str(e)}", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
